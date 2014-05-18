e014cfd7b5fc02d3078ea9ef504bef7f285c72f2
mjsteger <mjsteger1@gmail.com>
2014-05-17 20:38:49 -0700

    Adding newer rainbow mode stuff, and adding the first org mode file as an example

---
+++ modules/dreamacs-rainbow-delimiters.el
@@ -0,0 +1,17 @@
+
+;; We'll grab the rainbow delimiters package. I wonder what happens if this is over 80 characters. I really hope they do something smart
+
+(dreamacs-require-packages '(rainbow-delimiters))
+
+;; We'll want to use it everywhere, like so:
+
+(global-rainbow-delimiters-mode)
+(custom-set-variables
+ '(rainbow-delimiters-depth-1-face ((t (:foreground "red"))))
+ '(rainbow-delimiters-depth-2-face ((t (:foreground "green"))))
+ '(rainbow-delimiters-depth-3-face ((t (:foreground "violet"))))
+ '(rainbow-delimiters-depth-4-face ((t (:foreground "orange"))))
+ '(rainbow-delimiters-depth-5-face ((t (:foreground "brown"))))
+ '(rainbow-delimiters-unmatched-face ((t (:foreground "alternateSelectedControlColor")))))
+
+(provide 'dreamacs-rainbow-delimiters)
