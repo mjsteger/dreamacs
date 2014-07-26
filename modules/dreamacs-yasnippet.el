8c472c51256523c34e262aff7beec4b18fb4e338
Michael Steger <mjsteger1@gmail.com>
2014-07-25 17:42:31 -0700

    Add custom snippets

--- modules/dreamacs-yasnippet.el
+++ modules/dreamacs-yasnippet.el
@@ -1,3 +1,13 @@
-(dreamacs-require-packages '(yasnippet)) 
+(dreamacs-require-packages '(yasnippet))
+
+(setq yas-snippet-dirs
+      (list
+       (concat dreamacs-elpa-base "/" (car (directory-files dreamacs-elpa-base nil "yasnippet.*")) "/snippets")
+       (concat dreamacs-personal-dir "/snippets")
+       )
+      )
+
+(global-set-key (kbd "M-i") 'yas-expand)
+(yas-global-mode 1)
 
 (provide 'dreamacs-yasnippet)
