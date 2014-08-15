04764d6b5aaf1f2e462eea412dfc7884eb4d13ed
Michael Steger <mjsteger1@gmail.com>
2014-08-14 20:50:28 -0700

    Update erc configuration for images/nick highlighting

--- modules/dreamacs-erc.el
+++ modules/dreamacs-erc.el
@@ -1,3 +1,8 @@
-(dreamacs-require-packages '(znc))  
+(dreamacs-require-packages '(znc erc-image erc-hl-nicks))
+
+(add-to-list 'erc-modules 'image)
+(add-to-list 'erc-modules 'hl-nicks)
+(erc-update-modules)
+
 
 (provide 'dreamacs-erc)
