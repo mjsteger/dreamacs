5c6a86c54ed9fe0905796e7e349419920eb6f0c6
Michael Steger <mjsteger1@gmail.com>
2014-09-30 10:20:20 -0700

    Removing smart-parens, which blows up in larger files

--- core/dreamacs-editor.el
+++ core/dreamacs-editor.el
@@ -58,15 +58,6 @@ Will only occur if dreamacs-whitespace is also enabled."
 ;; smart tab behavior - indent or complete
 (setq tab-always-indent 'complete)
 
-;; smart pairing for all
-(require 'smartparens-config)
-(setq sp-base-key-bindings 'paredit)
-(setq sp-autoskip-closing-pair 'always)
-(setq sp-hybrid-kill-entire-symbol nil)
-(sp-use-paredit-bindings)
-
-(show-smartparens-global-mode +1)
-
 ;; disable annoying blink-matching-paren
 (setq blink-matching-paren nil)
 
