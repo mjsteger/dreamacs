554d861f091b6770c39d71076dd27b7014842be5
Michael Steger <mjsteger1@gmail.com>
2015-08-30 00:11:48 -0700

    Dear god add paredit

--- modules/dreamacs-clojure.el
+++ modules/dreamacs-clojure.el
@@ -1,4 +1,4 @@
-(dreamacs-require-packages '(cider))
+(dreamacs-require-packages '(cider paredit))
 
 (add-hook 'cider-mode-hook #'eldoc-mode)
 (setq nrepl-log-messages t)
@@ -8,3 +8,5 @@
 (add-hook 'cider-repl-mode-hook #'company-mode)
 (add-hook 'cider-mode-hook #'company-mode)
 (add-hook 'cider-repl-mode-hook #'paredit-mode)
+
+(provide 'dreamacs-clojure)
