6f99734d942648a5c3ac8d5eccacd0215a39d94d
Michael Steger <mjsteger1@gmail.com>
2018-08-19 15:24:22 -0700

    Committing change for wes on golang

--- modules/dreamacs-go.el
+++ modules/dreamacs-go.el
@@ -3,10 +3,15 @@
 (add-hook 'before-save-hook 'gofmt-before-save)
 
 (add-hook 'go-mode-hook '(lambda ()
-                           (set (make-local-variable 'company-backends) '(company-go))
+                           (set (make-local-variable 'company-backends) '(company-go . no-cache))
                            (company-mode)
+                           (setq godef-command (s-chomp (shell-command-to-string "which godef")))
           ))
 
-(with-eval-after-load 'go-mode (require 'go-autocomplete))
+(setq flycheck-go-golint-executable "/Users/steggy/gocode//bin/golint")
+
+;; (with-eval-after-load 'go-mode (require 'go-autocomplete))
+
+
 
 (provide 'dreamacs-go)
